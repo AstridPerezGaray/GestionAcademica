19a6c049d85f3f66ed83b6e9463a894f33e5b413
astrid <astridperezg3@gmail.com>
2024-05-23 22:59:44 -0600

    se termino guia 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EstudiantesService } from './services/estudiantes.service';
 import { CommonModule } from '@angular/common';
+import { CarrerasService } from './services/carreras.service';
+import { GruposService } from './services/grupos.service';
+import { ProfesoresService } from './services/profesores.service';
+import { MateriasService } from './services/materias.service';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +18,16 @@ export class AppComponent {
   title = 'GestionAcademica';
 
   private readonly estudiantesService = inject(EstudiantesService);
+  private readonly carreraService = inject(CarrerasService);
+  private readonly grupoService = inject(GruposService);
+  private readonly materiasService = inject(MateriasService);
+  private readonly profesorService = inject(ProfesoresService);
 
   //creando observable
   estudiantes$ = this.estudiantesService.obtenerEstdiantes();
+  carrera$ = this.carreraService.obtenerCarreras();
+  grupo$ = this.grupoService.obtenerGrupo();
+  materia$ = this.materiasService.obtenerMaterias();
+  profesor$ = this.profesorService.obtenerProfesor();
 }
 
